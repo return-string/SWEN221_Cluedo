05f7ecf5ccbeb1229bbe2baabe52e83a373aefe5
MicrowavedScrambledEggs <badi.t.james@gmail.com>
2015-07-30 17:22:30 +1200

    Card.java finished, TextUI.java still under construction

--- game/Card.java
+++ game/Card.java
@@ -8,51 +8,88 @@ package game;
  */
 
 public class Card {
-	public static enum Character { WHITE,SCARLET,MUSTARD,GREEN,PEACOCK,PLUM };
-	public static enum Weapon { CANDLESTICK, DAGGER, REVOLVER, ROPE, PIPE, WRENCH };
-	public static enum Room { CONSERVATORY, BALL, BILLIARD, DINING, HALL, KITCHEN, LOUNGE, STUDY };
+
+	public static enum Type {CHARACTER, WEAPON, ROOM};
 
 	public static final String WHITE = "Mrs White";
 	public static final String SCARLET = "Miss Scarlet";
 	public static final String MUSTARD = "Colonel Mustard";
 	public static final String GREEN = "Reverend Green";
 	public static final String PEACOCK = "Mrs Peacock";
+	public static final String PLUM = "Proffesor Plum";
 
-	public static final String CANDLESTICK = "Candlestick";
-	public static final String DAGGER = "Dagger";
-	public static final String
+	public static final String CANDLESTICK = "candlestick";
+	public static final String DAGGER = "dagger";
+	public static final String REVOLVER = "revolver";
+	public static final String ROPE = "rope";
+	public static final String PIPE = "pipe";
+	public static final String WRENCH = "wrench";
 
-	private final Character character;
-	private final Weapon weapon;
-	private final Room room;
+	public static final String CONSERVATORY = "conservatory";
+	public static final String BALL = "ball";
+	public static final String BILLIARD = "billiard";
+	public static final String DINING = "dining";
+	public static final String HALL = "hall";
+	public static final String KITCHEN = "kitchen";
+	public static final String LOUNGE = "lounge";
+	public static final String STUDY = "study";
 
-	public Card(Room r) {
-		room = r;
-		character = null;
-		weapon = null;
+	public static final String[] CHARACTERS = { WHITE,SCARLET,MUSTARD,GREEN,PEACOCK,PLUM };
+	public static final String[] WEAPONS = { CANDLESTICK, DAGGER, REVOLVER, ROPE, PIPE, WRENCH };
+	public static final String[] ROOMS = { CONSERVATORY, BALL, BILLIARD, DINING, HALL, KITCHEN, LOUNGE, STUDY };
+
+	public static final int DECKSIZE = 20;
+
+	private String value;
+	private Type type;
+
+	/**Constructor for class Card
+	 * Assumes type matches the value
+	 *@param type Type of card
+	 *@param value Value of card
+	 */
+	public Card(Type type, String value){
+		this.type = type;
+		this.value = value;
 	}
 
-	public Card(Weapon w) {
-		room = null;
-		character = null;
-		weapon = w;
+	public Type getType(){
+		return type;
 	}
 
-	public Card(Character c) {
-		room = null;
-		character = c;
-		weapon = null;
+	public String getValue(){
+		return value;
 	}
 
-	public boolean isCharacter() {
-		return character != null;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
 	}
 
-	public boolean isRoom() {
-		return room != null;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (type != other.type)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
 	}
 
-	public boolean isWeapon() {
-		return weapon != null;
-	}
+
+
+
 }
--- game/TextUI.java
+++ game/TextUI.java
@@ -1,15 +1,40 @@
 package game;
 
-/** 
- * 
+import java.util.Scanner;
+
+/**
+ *
  * @author jamesba
  *
  */
 public class TextUI {
 
-	public TextUI() {
-		
+	private Scanner scan;
+
+	public TextUI(){
+		scan = new Scanner(System.in);
 	}
 
-	public int read
+	/**Prints the inputed text to System.out
+	  *@param text Text to print out
+	  */
+	public void printText(String text){
+		System.out.println(text);
+	}
+
+	/**Prints the given message to System.out then
+	  *asks user for an interger. Can be used to get
+	  *user to select an option etc
+	  *@param question Message to print out
+	  *@return User inputed integer
+	  */
+	public int askInt(String question){
+		System.out.print(question);
+		return scan.nextInt();
+	}
+
+	public void printArray(String[] textArray){
+
+	}
+
 }
