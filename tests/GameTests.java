06dca086d35e2531373e74493290e4dd4e2c0e19
MicrowavedScrambledEggs <badi.t.james@gmail.com>
2015-08-01 12:00:00 +1200

    wrote a test for board.java

--- tests/GameTests.java
+++ tests/GameTests.java
@@ -6,11 +6,13 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import game.Board;
 import game.Card;
+import game.Coordinate;
 import game.Game;
 import game.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 /**
@@ -23,6 +25,14 @@ public class GameTests {
 		Board b = new Board();
 	}
 
+	@Test
+	public void test2_getMoves(){
+		Board b = new Board();
+		Map<Coordinate, String> moves = b.possibleMoves(new Coordinate(5,8), 6);
+		for(Coordinate coord : moves.keySet()){
+			System.out.printf("%s : %s\n", coord.toString(), moves.get(coord));
+		}
+	}
 
 	// ========= helper methods! =============
 
